6c467e0483f9b3561831e82fd06392a0c8885b43
Adnan Farooq <adnan057@yahoo.com>
2015-10-29 20:04:26 +0800

    modified code to real all videos and convert each video as a single array of frames and store in another numpy array videos

--- src/video_to_array.py
+++ src/video_to_array.py
@@ -1,29 +1,40 @@
+"""
+Reads each video file and stores each frames as an array , and store all the frames in the video list
+"""
+
 import numpy as np 
 import cv2
+import os
 
+filepath = 'data/'
 
-cap = cv2.VideoCapture('')
+videos = []
 
-frames = []
+for f in os.listdir(filepath):
+	if f.split('.')[-1] == 'avi':
+		if os.path.isfile(filepath + str(f)):
+			cap = cv2.VideoCapture(filepath + str(f))
 
-while True:
+			frames = []
 
-	ret, frame = cap.read()
+			while True:
+				ret, frame = cap.read()
 
-	if frame is None:
-		break
+				if frame is None:
+					break
 
-	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+				gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+				cv2.imshow('action',gray)
+				frames.append(gray)
+				cv2.waitKey(10)
 
-	cv2.imshow('walk',gray)
+			frames = np.array(frames)
+			videos.append(frames)
+			cap.release()
+		
+videos = np.array(videos)
+			
+#print(videos)
+#print(len(videos))
 
-	frames.append(gray)
-
-	cv2.waitKey(10)
-
-frames = np.array(frames)
-
-print(frames)
-
-cap.release()
 cv2.destroyAllWindows()
